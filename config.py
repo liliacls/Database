edd7fab920965eb64edbf79fc39b7b9465d6bcac
liliacls <chals.lilia@gmail.com>
2026-05-28 13:51:06 +0200

    Ajout du chemin du fichier d'historique à la racine du projet

--- config.py
+++ config.py
@@ -4,6 +4,7 @@ from sqlalchemy import create_engine
 
 PROJECT_ROOT = Path(__file__).parent
 DB_PATH = f"sqlite:///{PROJECT_ROOT / 'BacLipidDB.db'}"
+HISTORY_PATH = PROJECT_ROOT / "import_history.json"
 
 
 @st.cache_resource
