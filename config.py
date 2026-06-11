4e9a988aa3ebe0ae68849f13f992476d100573ad
liliacls <chals.lilia@gmail.com>
2026-06-11 11:46:36 +0200

    correction du fcihier

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ from sqlalchemy import create_engine
 
 PROJECT_ROOT = Path(__file__).parent
 DB_PATH = f"sqlite:///{PROJECT_ROOT / 'BacLipidDB.db'}"
-HISTORY_PATH = PROJECT_ROOT / "import_history.json"
+HISTORY_PATH = PROJECT_ROOT / "history.json"
 
 
 @st.cache_resource
