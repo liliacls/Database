4046827e4b044549ca58b5232c3517150a09e849
liliacls <chals.lilia@gmail.com>
2026-07-01 16:05:11 +0200

    Mise à jour de la docstrings en anglais

--- utils/precursor_type.py
+++ utils/precursor_type.py
@@ -1,12 +1,12 @@
 def precursor_type(neutral_mass: float, precursor_mz: float) -> str:
     """
-    Déduit le type de précurseur à partir de la masse neutre et du rapport m/z du précurseur.
+    Deduce the precursor type from the neutral mass and the precursor m/z ratio.
 
-    :param neutral_mass: masse neutre calculée (Daltons).
+    :param neutral_mass: calculate neutral mass (Daltons).
     :type neutral_mass: float
-    :param precursor_mz: rapport m/z du précurseur.
+    :param precursor_mz: precursor m/z ratio (Daltons).
     :type precursor_mz: float
-    :return: "[M-H]-" si mode négatif, "[M+H]+" si mode positif.
+    :return: "[M-H]-" if neutral_mass > precursor_mz, "[M+H]+" otherwise
     :rtype: str
     """
     return "[M-H]-" if neutral_mass > precursor_mz else "[M+H]+"
